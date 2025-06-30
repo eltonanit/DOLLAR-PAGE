a77659101e45b66e6d02aef7cb6c9df9487cc6bd
eltonanit <eltonanit@gmail.com>
2025-06-30 18:16:28 +0200

    Final architecture fix.

--- setup_database.js
+++
@@ -1,9 +0,0 @@
-const { setupDatabase } = require('./database.js');
-
-console.log("Esecuzione dello script di setup del database...");
-
-setupDatabase();
-
-console.log("Setup del database completato. La tabella 'counters' è stata creata (se non esisteva già).");
-
-// Nota: questo script è molto semplice. Non ha bisogno di rimanere in esecuzione.
